bfd6e1d83fae5083d8d223179cb46023c50b325d
ceb <vivu1991@mail.ru>
2022-09-03 16:34:34 +0300

    DZ 001 решение первой задачи

---
+++ DZ001/Program.cs
@@ -0,0 +1,15 @@
+﻿void stAB()
+{
+    Console.Write("Введите число А: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число В: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = a;
+
+    for (int i = 1; i < b; i++)
+    {
+        c = c * a;
+    }
+    Console.WriteLine($"Число А с степени числа В равно: {c}");
+}
+stAB();
