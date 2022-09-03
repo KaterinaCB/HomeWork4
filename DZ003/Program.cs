bf7f4f6529e883cdcb4e6f0afe12df1c7e7b5af5
ceb <vivu1991@mail.ru>
2022-09-03 17:18:25 +0300

    DZ 03 решение третьей задачи

---
+++ DZ003/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] array = new int [8];
+
+void FillArray (int [] mass)
+{
+    int length = mass.Length;
+    int index = 0;
+    while (index < length)
+    {
+        mass [index] = new Random().Next(0,100);
+        index++;
+    }
+}
+void PrintArray (int [] mass)
+{
+    int count = mass.Length;
+    for (int position = 0; position < count; position++)
+    {
+        if (position != count - 1)
+        Console.Write($"{mass[position]}, ");
+        else Console.Write($"{mass[position]}");
+        
+    }
+}
+FillArray(array);
+PrintArray(array);
