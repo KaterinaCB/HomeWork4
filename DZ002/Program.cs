4f1432b991854faf7c622553bd466cb3ff572081
ceb <vivu1991@mail.ru>
2022-09-03 16:47:22 +0300

    DZ 002 решение второй задачи

---
+++ DZ002/Program.cs
@@ -0,0 +1,15 @@
+﻿void Summ()
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+
+    while (x > 0)
+    {
+        int num = x % 10;
+        x = x / 10;
+        sum = sum + num;
+    }
+    Console.WriteLine($"Сумма цифр числа равна: {sum}");
+}
+Summ();
